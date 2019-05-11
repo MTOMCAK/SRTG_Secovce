3681b2bba886ff44c33c70d38aa755c3b8307a89
mtomcak <tomcak.matus@gmail.com>
2019-05-11 23:36:01 +0200

    SRTG Single Page App - scroll template done

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-var element = React.createElement('h1', {className: 'greeting'}, 'Hello, World!');
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
